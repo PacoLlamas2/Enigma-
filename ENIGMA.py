399453e88e9b46c02f48bff29fe86cc38c365a86
pacollamas <francisco.llamas@estudiant.enti.cat>
2025-12-05 00:10:42 +0100

    Estructura inicial

---
+++ ENIGMA.py
@@ -0,0 +1,29 @@
+from funcions_engima import *
+def mostrar_menu():
+    print("\nENIGMA:")
+    print("-------------------------------")
+    print("1. Xifrar missatge")
+    print("2. Desxifrar missatge")
+    print("3. Editar rotors")
+    print("4. Sortir")
+
+def main():
+    seguir = True
+    while seguir:
+        mostrar_menu()
+        opcio = input("Selecciona una opció: ")
+
+        if opcio == '1':
+            xifrar_missatge()
+        elif opcio == '2':
+            desxifrar_missatge()
+        elif opcio == '3':
+            ##EMPIEZO POR ESTE 
+            editar_rotors() 
+        elif opcio == '4':
+            print("Sortint...")
+            seguir = False
+        else:
+            print("Opció no vàlida, torna-ho a provar.")
+
+main()
